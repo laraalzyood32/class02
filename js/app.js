bc9794727ed122f66136ca685537b73a2e9899ea
alzyoodlara89 <lara.ma7mood2012@gmail.com>
2021-03-02 10:07:00 +0200

    class02 conssit many files we code tree to list them

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let username='lara';
+let age=30;
+
+let res = ans1.toUpperCase();
+
+
+
+function rep (ans1,ans2)
+console.log("welcom");
+
+console.log("guessing game about me");
+
+let user=prompt('my favourite snak is banana?');
+if (user=='y' || user=='Y' || user=='YES' || user==yes)
+{
+
+
+
+    alert('this is wrong');
+   
+    }
+    else 
+    {
+        alert('this is correct');
+    
+    }
+    
+
+        
+    
+let userage=prompt("my age is 30?");
+if (user==ans1)
+{
+    alert('this is correct');
+    }else 
+    {
+        alert('this is wrong');
+    }
+        
+
+    
+let userInput=prompt('does i love winter: ');
+if (user==ans1)
+{
+    alert('this is wrong');
+    }else
+    {
+        alert('this is correct');
+    }
+
+ 
+
+let userout=prompt("enter your name");
+
+alert ("you win");
+alert('welcome ',USEROUT);
